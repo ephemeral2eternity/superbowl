0106a49089ea6a6b8cbb9f0ae0bedd6b8121886c
Ubuntu <18757@client-43.1qkwgmudbbrutlxo3k0lkexymg.cx.internal.cloudapp.net>
2017-02-05 21:04:02 +0000

    update check_status.sh

--- check_status.sh
+++ check_status.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
 echo "Probing sites timestamp:"
-cat ~/superbowl/probe/*_ping.csv |tail -1|awk -F',' '{print $1}'
+cat /home/18757/superbowl/probe/*_ping.csv |tail -1|awk -F',' '{print $1}'
 echo "Probing servers timestamp:"
-cat ~/superbowl/probe/*_cacheping.csv |tail -1|awk -F',' '{print $1}'
+cat /home/18757/superbowl/probe/*_cacheping.csv |tail -1|awk -F',' '{print $1}'
 echo "Traceroute servers timestamp:"
-ls -tr ~/superbowl/trace/ |tail -1
+ls -la ~/superbowl/trace/ |tail -1|awk -F'_' '{print $2}'
+echo "END"
