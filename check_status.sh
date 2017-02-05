26ea714a52db2bc3fd195dd5cebc25e0c0e44163
Chen W <chenw@cmu.edu>
2017-02-05 18:07:44 -0500

    Add revisions to check status

--- check_status.sh
+++ check_status.sh
@@ -3,5 +3,5 @@ cat ~/superbowl/probe/*_ping.csv |tail -1|awk -F',' '{print $1}'
 echo "Probing servers timestamp:"
 cat ~/superbowl/probe/*_cacheping.csv |tail -1|awk -F',' '{print $1}'
 echo "Traceroute servers timestamp:"
-ls -la ~/superbowl/trace/ |tail -1|awk -F'_' '{print $2}'
+ls -tr ~/superbowl/trace/ |tail -1|awk -F'_' '{print $2}'
 echo "END"
