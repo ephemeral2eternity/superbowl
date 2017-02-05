6ac0d00ac39c10a883d696d9b8b596c4ecd31ab6
Ubuntu <18757@client-43.1qkwgmudbbrutlxo3k0lkexymg.cx.internal.cloudapp.net>
2017-02-05 20:52:27 +0000

    Change folder for check status

--- check_status.sh
+++ check_status.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Probing sites timestamp:"
-cat ./probe/*_ping.csv |tail -1|awk -F',' '{print $1}'
+cat ~/superbowl/probe/*_ping.csv |tail -1|awk -F',' '{print $1}'
 echo "Probing servers timestamp:"
-cat ./probe/*_cacheping.csv |tail -1|awk -F',' '{print $1}'
+cat ~/superbowl/probe/*_cacheping.csv |tail -1|awk -F',' '{print $1}'
 echo "Traceroute servers timestamp:"
-ls -tr ./trace/ |tail -1
+ls -tr ~/superbowl/trace/ |tail -1
