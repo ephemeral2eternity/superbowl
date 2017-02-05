8bb4a9bb9163f7527f8e10d2a9f220b2fdb9719a
Chen W <chenw@cmu.edu>
2017-02-05 16:42:42 -0500

    revise check_status.sh

--- check_status.sh
+++ check_status.sh
@@ -1,7 +1,7 @@
 echo "Probing sites timestamp:"
-cat /home/18757/superbowl/probe/*_ping.csv |tail -1|awk -F',' '{print $1}'
+cat ~/superbowl/probe/*_ping.csv |tail -1|awk -F',' '{print $1}'
 echo "Probing servers timestamp:"
-cat /home/18757/superbowl/probe/*_cacheping.csv |tail -1|awk -F',' '{print $1}'
+cat ~/superbowl/probe/*_cacheping.csv |tail -1|awk -F',' '{print $1}'
 echo "Traceroute servers timestamp:"
 ls -la ~/superbowl/trace/ |tail -1|awk -F'_' '{print $2}'
 echo "END"
